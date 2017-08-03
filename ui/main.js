1c09cdb087c33b2e21e054dbce078f5c93f28b00
KarthikaSaran <30622560+KarthikaSaran@users.noreply.github.com>
2017-08-03 19:30:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,5 +2,5 @@ var element=document.getElementById("textq");
 element.innerHTML="Hi";
 var img1=document.getElementById('img1');
 img1.onClick=function(){
-    img1.style.marginleft="100px";
+    img1.style.marginleft="160px";
 };
