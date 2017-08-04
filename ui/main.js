a2dfc5deb91e7eff5a7a7709af90534be740ee7b
KarthikaSaran <30622560+KarthikaSaran@users.noreply.github.com>
2017-08-04 14:45:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,11 @@
 var element=document.getElementById("textq");
 element.innerHTML="Hi";
 var img=document.getElementById("img1");
-console.log(img.style.marginLeft);
+console.log(img.marginLeft);
 var marginLeft=0;
 function moveRight(){
     marginLeft=marginLeft+10;
-    img.style.margin.left=marginLeft+"px";
+    img.marginLeft=marginLeft+"px";
 }
 img.onClick=function(){
     var interval=setInterval(moveRight(),50);
