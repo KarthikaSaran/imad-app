fe77ba1a5e71352bf6b627567a2e81924e738dc9
KarthikaSaran <30622560+KarthikaSaran@users.noreply.github.com>
2017-08-04 14:38:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 var element=document.getElementById("textq");
 element.innerHTML="Hi";
 var img=document.getElementById("img1");
-console.log(img.style.marginRight);
+console.log(img.style.paddingLeft);
 var marginLeft=0;
 function moveRight(){
     marginLeft=marginLeft+10;
