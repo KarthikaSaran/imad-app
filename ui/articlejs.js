e9dda75dd2d68f598d4cd48e38e4eb101bb1fa50
KarthikaSaran <30622560+KarthikaSaran@users.noreply.github.com>
2017-08-07 17:31:01 +0530

    Create articlejs.js

---
+++ ui/articlejs.js
@@ -0,0 +1,14 @@
+var button3=document.getElementById("comment");
+button3.onclick=function(){
+	var comm=document.getElementById("comm").value;
+	document.getElementById("comm").value='';
+	var request=new XMLHttpRequest();
+    var articleName=this.document.title.split(" ")[0];
+    console.log(articleName);
+    articleName[0]='a';
+	request.open('GET','http://karthikaraghavendrar7.imad.hasura-app.io/'+articleName+'/submitcomment/?comment='+comm,true);
+    request.send(null); 
+
+
+	
+};
