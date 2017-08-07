85d24b645615f276e23ea1cd5b06d5c4b8571ca5
KarthikaSaran <30622560+KarthikaSaran@users.noreply.github.com>
2017-08-07 17:41:37 +0530

    [imad-console] Updates ui/articlejs.js

--- ui/articlejs.js
+++ ui/articlejs.js
@@ -3,8 +3,8 @@ button3.onclick=function(){
 	var comm=document.getElementById("comm").value;
 	document.getElementById("comm").value='';
 	var request=new XMLHttpRequest();
-    var articleName=document.title.split(" ")[0];
-    articleName[0]='a';
+    var articleName=document.title.split(" ")[0].toLowerCase();
+    
     console.log(articleName);
 	request.open('GET','http://karthikaraghavendrar7.imad.hasura-app.io/'+articleName+'/submitcomment/?comment='+comm,true);
     request.send(null); 
