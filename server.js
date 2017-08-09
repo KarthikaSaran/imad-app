99dabe52079216cab645fcba37cef524e99aed6e
KarthikaSaran <30622560+KarthikaSaran@users.noreply.github.com>
2017-08-09 14:45:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -154,7 +154,7 @@ app.get("/:articleName",function (req,res){
 });
 
 app.get("/articles/:articleName",function (req,res){
-   pool.query("SELECT * FROM article where title="+req.params.articleName,function(err,result){
+   pool.query("SELECT * FROM article where title="+"'"+req.params.articleName+"'",function(err,result){
       if(err) res.status(500).send(err.toString());
       else if(result.rows.length===0) res.status(404).send("Not found");
       else res.send(createTemplate(result.rows[0]));
