8963c2a2d5aad73214869cc886ff44fc965ad45f
KarthikaSaran <30622560+KarthikaSaran@users.noreply.github.com>
2017-08-16 11:24:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -129,7 +129,7 @@ var config={
 }*/
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'abcd.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 var pool=new Pool(config);
 app.get('/test-db',function(req,res) {
