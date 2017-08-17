00587eebef6428009b81891abb8495e7032ee075
KarthikaSaran <30622560+KarthikaSaran@users.noreply.github.com>
2017-08-17 22:35:59 +0530

    [imad-console] Updates ui/welcomejs.js

--- ui/welcomejs.js
+++ ui/welcomejs.js
@@ -1,1 +1,12 @@
-
+var button = document.getElementById("signout");
+button.onclick = function(){
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function() {
+         
+         if(request.readyState===XMLHttpRequest.DONE) {
+            if(request.status===200) 
+                window.location.href='http://karthikaraghavendrar7.imad.hasura-app.io/logout';
+        }
+     
+  }; 
+};
