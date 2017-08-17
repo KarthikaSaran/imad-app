e770c0b5e15e4b066a54f8ea6a79990c05f99525
KarthikaSaran <30622560+KarthikaSaran@users.noreply.github.com>
2017-08-17 20:34:17 +0530

    [imad-console] Updates ui/regjs.js

--- ui/regjs.js
+++ ui/regjs.js
@@ -5,14 +5,12 @@ button5.onclick = function(){
   var age = document.getElementById("age").value;
   var password = document.getElementById("password").value;
   var confirmpassword = document.getElementById("confirmpassword").value;
+  console.log(name+password);
   if (password === confirmpassword) {
     var request=new XMLHttpRequest();
     request.onreadystatechange=function() {
         if(request.readyState===XMLHttpRequest.DONE) {
-           if(request.status===200) alert("Login Successful");
-           else if(request.status===403) alert("Login Failed");
-           else if(request.status===404) alert("User not available");
-           else if(request.status===500) alert("Something went wrong");
+           if(request.status===200) alert("user created successfully");
         }
     request.open('POST','http://karthikaraghavendrar7.imad.hasura-app.io/create-user',true);
     request.setRequestHeader('Content-type','application/json');
